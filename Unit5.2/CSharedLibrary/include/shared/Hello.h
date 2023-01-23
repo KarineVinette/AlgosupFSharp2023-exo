3e6b7d1a7c70e18e175e2cdd95e3aaceda98c893
Robert Pickering <robert@strangelights.com>
2023-01-23 21:30:19 +0100

    minor improvements

--- Unit5.2/CSharedLibrary/include/shared/Hello.h
+++ Unit5.2/CSharedLibrary/include/shared/Hello.h
@@ -14,25 +14,20 @@ struct Point
 #ifdef _WIN32
     __declspec(dllexport)
 #endif
-    void print_text();
-#ifdef _WIN32
-    __declspec(dllexport)
-#endif
-    char* make_char_buffer(int32_t number);
-#ifdef _WIN32
-    __declspec(dllexport)
-#endif
-    void free_char_buffer(char* heap_text);
-#ifdef _WIN32
-    __declspec(dllexport)
-#endif
     void int_pointer_demo(int32_t* number);
-#ifdef _WIN32
-    __declspec(dllexport)
-#endif
-    void struct_demo(Point* point);
+
 #ifdef _WIN32
     __declspec(dllexport)
 #endif
     void char_buffer_demo(char* buffer, int32_t* char_count);
+
+#ifdef _WIN32
+    __declspec(dllexport)
+#endif
+    void struct_demo(Point* point);
+
+#ifdef _WIN32
+    __declspec(dllexport)
+#endif
+    void struct_buffer_demo(Point* points, int32_t* elements);
 }
--- Unit5.2/FsProgram/Program.fs
+++ Unit5.2/FsProgram/Program.fs
@@ -26,64 +26,95 @@ module Program =
 
     let IntPtrDemo() =
         Console.WriteLine("IntPtrDemo")
+
+        // target value
         let mutable i = 0
 
+        // prepare the pointer
         let iPtr = &&i
+        let nint = NativePtr.toNativeInt iPtr
 
-        Native.int_pointer_demo(NativePtr.toNativeInt iPtr)
+        // call the native method
+        Native.int_pointer_demo(nint)
 
+        // print the result
         Console.WriteLine(i)
 
     let CharBufferDemo() =
         Console.WriteLine("CharBufferDemo")
 
-        let mutable length = 0
-
-        let lengthPtr = &&length
-
+        // buffer to hold the string
         let buffer = Marshal.AllocHGlobal(512)
 
-        Native.char_buffer_demo(buffer, NativePtr.toNativeInt lengthPtr)
+        // target value to hold the length
+        let mutable length = 0
 
-        let stringCopiedFromNative = Marshal.PtrToStringAnsi(buffer, length);
+        // prepare the pointer
+        let lengthPtr = &&length
+        let nint = NativePtr.toNativeInt lengthPtr
 
+
+        // call the native method
+        Native.char_buffer_demo(buffer, nint)
+
+        // convert the unmanaged data
+        let stringCopiedFromNative =
+            Marshal.PtrToStringAnsi(buffer, length);
+
+        // free the memory we allocated
         Marshal.FreeHGlobal(buffer)
 
+        // print the result
         Console.WriteLine(stringCopiedFromNative)
 
     let StructDemo() =
         Console.WriteLine("StructDemo")
 
+        // target struct
         let mutable p = { X = 0; Y = 0 }
 
+        // prepare the pointer
         let pPtr = &&p
+        let nint = NativePtr.toNativeInt pPtr
 
-        Native.struct_demo(NativePtr.toNativeInt pPtr)
+        // call the native method
+        Native.struct_demo(nint)
 
+        // print the result
         Console.WriteLine(p)
 
     let StructBufferDemo() =
         Console.WriteLine("StructBufferDemo")
 
+        // find the size of our struct
         let size = Marshal.SizeOf(typeof<Point>)
 
+        // allocate a buffer large enough to hold the results
         let buffer = Marshal.AllocHGlobal(size * 25)
 
+        // target value to hold the length
         let mutable length = 0
 
+        // prepare the pointer
         let lengthPtr = &&length
+        let nint = NativePtr.toNativeInt lengthPtr
 
-        Native.struct_buffer_demo(buffer, NativePtr.toNativeInt lengthPtr)
+        // call the native method
+        Native.struct_buffer_demo(buffer, nint)
 
-        Console.WriteLine(length)
 
-
+        // create a .NET array to hold the results
         let (result: Point[]) = Array.zeroCreate length
 
+        // loop over the structs converting them to .NET
         for i in 0 .. length - 1 do
             let structIndex: nativeint = buffer + (nativeint i * nativeint size)
             result.[i] <- Marshal.PtrToStructure<Point>(structIndex)
 
+        // free the memory we allocated
+        Marshal.FreeHGlobal(buffer)
+
+        // print the result
         Console.WriteLine(sprintf "%A" result)
 
 
